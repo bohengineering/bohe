5f1b03875442abe2ae470ea816929b016387a292
bohengineering <kai@bohengineering.com>
2025-12-28 23:54:53 -0500

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <section class="bg-slate-200 text-slate-700 p-8 rounded-md">
       <h2 class="text-2xl">Home</h2>
-      <p class="mt-4">Back of House Engineering</p>
+      <p class="mt-4">Coming soon</p>
 
       <div class="flex items-center space-x-2 mt-4">
         <button
